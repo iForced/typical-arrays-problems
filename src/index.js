1d1d42049ac9dedede329539820de66c7c61ca04
iForced <iforce@2ch.hk>
2021-02-25 22:33:09 +0300

    fix: check params

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 
 exports.min = function (array) {
-  if (array.length == 0) {
-    return 0;
-}
-let res = Number.POSITIVE_INFINITY;
-array.forEach(item => {
-    if (item < res) {
-        res = item;
-    }
-})
-return res;
+    if (!(array.length) || array.length == 0) {
+      return 0;
+  }
+  let res = Number.POSITIVE_INFINITY;
+  array.forEach(item => {
+      if (item < res) {
+          res = item;
+      }
+  })
+  return res;
 }
 
 exports.max = function (array) {
-  if (array.length == 0) {
-    return 0;
-}
-let res = Number.NEGATIVE_INFINITY;
-array.forEach(item => {
-    if (item > res) {
-        res = item;
-    }
-})
-return res;
+    if (!(array.length) || array.length == 0) {
+      return 0;
+  }
+  let res = Number.NEGATIVE_INFINITY;
+  array.forEach(item => {
+      if (item > res) {
+          res = item;
+      }
+  })
+  return res;
 }
 
 exports.avg = function (array) {
-  if (array.length == 0) {
-    return 0;
-}
-let res = 0;
-array.forEach(item => {
-    res += item;
-})
-res = res / arr.length;
-return res;
+    if (!(array.length) || array.length == 0) {
+      return 0;
+  }
+  let res = 0;
+  array.forEach(item => {
+      res += item;
+  })
+  res = res / array.length;
+  return res;
 }
